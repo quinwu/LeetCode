28791e8a914f74d65b8949bd015b3ca2226c86b1
quinwu <wukuan1995@outlook.com>
2018-01-30 17:09:48 +0800

    add 027

---
+++ 027. Remove Element/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        flag = 0
+        for index in range(len(nums)):
+            if nums[index] != val:
+                nums[flag] = nums[index]
+                flag += 1
+
+        return flag
